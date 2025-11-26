dbb2e68dc95bde48c1cbc44e8d6280e23a8a9efc
Luis Araujo <luisaraujo.dev@gmail.com>
2025-11-26 06:47:44 -0300

    refactore: logo

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -32,9 +32,8 @@ export default async function PublicLayout({
               <span className="text-xs text-muted font-medium uppercase tracking-wide">
                 um projeto de <span className="text-white font-bold normal-case ml-1">Luís Araújo</span>
               </span>
-              <Image src="/logo.png" alt="Logo" fill className="object-cover" />
-              <div className="relative size-6 rounded-full bg-white flex items-center justify-center overflow-hidden ring-2 ring-white/10">
-                <div className="size-3 bg-black rounded-full"></div>
+              <div className="relative size-8 flex items-center justify-center overflow-hidden">
+                              <Image src="/logo.png" alt="Logo" fill className="object-cover" />
               </div>
             </div>
 
